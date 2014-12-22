b2fff4f194d9cf54f4e0a1decfab054f2704c124
wangxiaohui <wangxiaohui@jiuzhilan.net>
2014-12-22 12:30:54 +0800

    update README & add one example

--- example/example.go
+++ example/example.go
@@ -1,12 +1,3 @@
-# cmgo
-
-A simple mgo client to use mongodb
-
-# Example
-
-Example [program](examples/example.go) demonstrates how to use the cmgo.
-
-```go
 package main
 
 import (
@@ -45,21 +36,4 @@ func main () {
 
     fmt.Println(ms)
 }
-```
 
-# Install
-
-`go get github.com/ewangplay/cmgo`
-
-Use `go get -u` to update the package.
-
-# Tests
-
-Run `go test cmgo` to run test on cmgo
-and `go bench` for benchmarks
-
-## License
-
-The [BSD 3-Clause license][bsd], the same as the [Go language][golang].
-[bsd]: http://opensource.org/licenses/BSD-3-Clause
-[golang]: http://golang.org/LICENSE
